5c124c72a0bc8ab1135dfa7ba66c51bc35631bb8
AKSHAT VIDYARTHI <akshat.vid@gmail.com>
2021-04-21 02:01:53 +0530

    functional getplasma.php

---
+++ Personal/forms/getplasma.php
@@ -0,0 +1,41 @@
+<?php
+$con = mysqli_connect("localhost","root","");
+if (!$con)
+{
+    die("Error Connecting to DB".mysqli_connect_error());
+}
+$db=mysqli_select_db($con,"VOLUNTEER");
+
+$st = $_POST['p_state'];
+$ci = $_POST['p_city'];
+$bl = $_POST['p_blood'];
+
+$sql = "SELECT * FROM Volunteer_Detail WHERE d_state='$st' and d_city='$ci' and blood_gp='$bl'";
+
+$res = mysqli_query($con, $sql);
+
+if ($res)
+{
+    // echo "$st, $ci, $bl";
+    echo "<table border='2'> <thead> <th>NAME</th> <th>PHONE NUMBER</th> <th>AGE</th> <th>E-MAIL</th> <th>STATE</th> <th>CITY</th> <th>BLOOD GROUP</th> <th>RECOVERED</th> </thead>";
+    while($row = mysqli_fetch_array($res)) 
+    {
+        // print_r($row);
+        echo "<tr>";
+        echo "<td>". $row['full_name']."</td>";
+        echo "<td>". $row['phone_no']."</td>"; 
+        echo "<td>". $row['d_age']."</td>";
+        echo "<td>". $row['e_mail']."</td>";
+        echo "<td>". $row['d_state']."</td>";
+        echo "<td>". $row['d_city']."</td>";
+        echo "<td>". $row['blood_gp']."</td>";
+        echo "<td>". $row['recovered']."</td>";
+        echo "</tr>"; 
+    }
+    echo "</table>";
+}
+else
+{
+    echo "Error:". mysqli_error($con);
+}
+mysqli_close($con);
