2340661ac19e0e363e5c0864d5f4d308e6f5599c
AKSHAT VIDYARTHI <akshat.vid@gmail.com>
2021-04-20 22:50:25 +0530

    update 2.0

---
+++ Personal/forms/createtable.php
@@ -0,0 +1,20 @@
+<?php
+$con = mysqli_connect("localhost","root","");
+if (!$con)
+{
+    die("Error Connecting to DB".mysqli_connect_error());
+}
+$db=mysqli_select_db($con,"VOLUNTEER");
+
+$sql = "CREATE table Volunteer_Detail (full_name varchar(25), phone_no bigint(10), d_age int(2), e_mail varchar(30), d_state varchar(20), d_city varchar(15), blood_gp varchar(4), recovered varchar(12))";
+
+
+if (mysqli_query($con, $sql) )
+{
+    echo "Table Created";
+}
+else
+{
+    echo "Error:". mysqli_error($con);
+}
+mysqli_close($con);
