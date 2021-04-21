83f7e89c82ff2bd754875203767cb57c9f610831
AKSHAT VIDYARTHI <akshat.vid@gmail.com>
2021-04-21 11:50:03 +0530

    update 5.0

--- Personal/forms/getplasma.php
+++ Personal/forms/getplasma.php
@@ -16,23 +16,32 @@ $res = mysqli_query($con, $sql);
 
 if ($res)
 {
-    // echo "$st, $ci, $bl";
-    echo "<table border='2'> <thead> <th>NAME</th> <th>PHONE NUMBER</th> <th>AGE</th> <th>E-MAIL</th> <th>STATE</th> <th>CITY</th> <th>BLOOD GROUP</th> <th>RECOVERED</th> </thead>";
-    while($row = mysqli_fetch_array($res)) 
+    if (mysqli_num_rows($res)>0)
     {
-        // print_r($row);
-        echo "<tr>";
-        echo "<td>". $row['full_name']."</td>";
-        echo "<td>". $row['phone_no']."</td>"; 
-        echo "<td>". $row['d_age']."</td>";
-        echo "<td>". $row['e_mail']."</td>";
-        echo "<td>". $row['d_state']."</td>";
-        echo "<td>". $row['d_city']."</td>";
-        echo "<td>". $row['blood_gp']."</td>";
-        echo "<td>". $row['recovered']."</td>";
-        echo "</tr>"; 
+        // echo "$st, $ci, $bl";
+        echo "<table border='2'> <thead> <th>NAME</th> <th>PHONE NUMBER</th> <th>AGE</th> <th>E-MAIL</th> <th>STATE</th> <th>CITY</th> <th>BLOOD GROUP</th> <th>RECOVERED</th> </thead>";
+        while($row = mysqli_fetch_array($res)) 
+        {
+            // print_r($row);
+            echo "<tr>";
+            echo "<td>". $row['full_name']."</td>";
+            echo "<td>". $row['phone_no']."</td>"; 
+            echo "<td>". $row['d_age']."</td>";
+            echo "<td>". $row['e_mail']."</td>";
+            echo "<td>". $row['d_state']."</td>";
+            echo "<td>". $row['d_city']."</td>";
+            echo "<td>". $row['blood_gp']."</td>";
+            echo "<td>". $row['recovered']."</td>";
+            echo "</tr>"; 
+        }
+        echo "</table>";
     }
-    echo "</table>";
+    else
+    {
+        echo "Required Type Not Found <br>";
+        echo "Don't Worry Apply For Requirement <div><button type='submit'>Apply</button></div>";
+    }
+    
 }
 else
 {
